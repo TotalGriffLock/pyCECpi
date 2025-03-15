6d277897e8b622022d5e11752b72bb39faa51a5c
TotalGriffLock <john@theshado.ws>
2025-03-15 16:33:14 +0000

    Create clearoled.py
    A script to clear the OLED  screen on shutdown (in case the pi stays powered)

---
+++ usr/local/bin/clearoled.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2017 Tony DiCola for Adafruit Industries
+# SPDX-FileCopyrightText: 2017 James DeVito for Adafruit Industries
+# SPDX-License-Identifier: MIT
+# Additions for the pyCECpi project, see https://github.com/TotalGriffLock/pyCECpi
+#
+# This script clears the OLED screen on shutdown
+
+from board import SCL, SDA
+import busio
+import adafruit_ssd1306
+
+# Create the I2C interface.
+i2c = busio.I2C(SCL, SDA)
+
+# Create the SSD1306 OLED class.
+# The first two parameters are the pixel width and pixel height.  Change these
+# to the right size for your display!
+disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+
+# Clear display.
+disp.fill(0)
+disp.show()
